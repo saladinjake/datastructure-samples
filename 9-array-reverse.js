3f54f9a0efa22ea72af0626288cd27b6b8673624
saladinjake <juwavictor@gmail.com>
2022-06-22 11:49:30 +0100

    array reverse

---
+++ 9-array-reverse.js
@@ -0,0 +1,11 @@
+
+const reverseArr = array =>{
+ for (let i = 0; i < array.length / 2; i++) {
+    const temp = array[i];
+    array[i] = array[array.length - 1 - i];
+    array[array.length - 1 - i] = temp;
+  }
+  return array;
+}
+
+console.log(reverseArr([1, 2, 3, 4, 5, 6])
